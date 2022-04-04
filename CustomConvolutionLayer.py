8ba4e343f63baad1a5aa0a8b79711f6dd11740e8
viibrem <rashaad.meyer@gmail.com>
2022-04-04 15:44:20 +0200

    Implemented Custom Convolution Layer but is slow. Will need to change nested for loops

--- CustomConvolutionLayer.py
+++ CustomConvolutionLayer.py
@@ -10,8 +10,9 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 class CustomConvolutionLayer(layers.Layer):
     def __init__(self, kernel_size):
         super(CustomConvolutionLayer, self).__init__()
+        self.weight_matrix = None
         self.kernel_size = kernel_size
-        self.w = self.add_weight(
+        self.kernel = self.add_weight(
             name="w",
             shape=(kernel_size, kernel_size),
             dtype=tf.float32,
@@ -19,53 +20,58 @@ class CustomConvolutionLayer(layers.Layer):
             trainable=True,
         )
 
+    def build(self, input_shape):
+        # create toeplitz to matmul with
+        input_dim = tf.cast(input_shape[-1], tf.int32)
+        input_size = input_dim*input_dim
+
+        output_dim = input_dim - self.kernel_size + 1
+        output_size = output_dim*output_dim
+
+        row = tf.TensorArray(tf.float32, size=input_size, dynamic_size=False, clear_after_read=False)
+        zeros = tf.TensorArray(tf.float32, size=input_size, dynamic_size=False, clear_after_read=False)
+
+        for i in range(input_size):
+            if i % input_dim < self.kernel_size and i // input_dim < self.kernel_size:
+                row = row.write(i, self.kernel[i // input_dim][(i % input_dim) % self.kernel_size])
+            else:
+                row = row.write(i, 0)
+            if i == 0:
+                zeros = zeros.write(i, self.kernel[0][0])
+            else:
+                zeros = zeros.write(i, 0)
+
+        row = row.stack()
+        zeros = zeros.stack()
+
+        toeplitz = tf.linalg.LinearOperatorToeplitz(zeros, row).to_dense()
+
+        # Take a slices of toeplitz matrix because toeplitz is not exactly the same as the matrix that we need
+        temp = []
+        for i in range(input_size):
+            if not (i % input_dim > input_dim - self.kernel_size):
+                temp.append(toeplitz[i])
+
+            if len(temp) >= output_size:
+                break
+
+        self.weight_matrix = tf.concat([temp], 0)
+
     def call(self, inputs):
-        # do padding
-        pad = int((self.kernel_size - 1)/2)
-        padding = tf.constant([[0, 0], [pad, pad], [pad, pad]])
-        inputs = tf.pad(inputs, padding, "CONSTANT")
-
-        input_shape = inputs.shape
-        tf.print(input_shape)
-        batch_size = input_shape[0]
-
-        output_shape = input_shape[1] - self.kernel_size + 1
-
-        # iterate through data
-        output_temp = []
-        for c in range(batch_size):
-            # iterate through each pixel/data point
-            temp = []
-            for i in range(output_shape):
-                row = tf.TensorArray(tf.float32, size=output_shape, dynamic_size=False, clear_after_read=False)
-                for j in range(output_shape):
-                    element = tf.constant(0, dtype=tf.float32)
-                    for u in range(self.kernel_size):
-                        for v in range(self.kernel_size):
-                            n = tf.multiply(self.w[u][v], inputs[c][i + u][j + v])
-                            element = tf.add(element, n)
-
-                    row = row.write(j, element)
-                row = row.stack()
-                temp.append(row)
-
-            output_temp.append(tf.concat([temp], 0))
-        output = tf.concat([output_temp], 0)
-        return output
+        return tf.matmul(self.kernel_matrix, inputs)
 
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.astype("float32") / 255.0
-x_test = x_test.astype("float32") / 255.0
+x_train = x_train.reshape(-1, 28 * 28).astype("float32") / 255.0
+x_test = x_test.reshape(-1, 28 * 28).astype("float32") / 255.0
 
 # Sequential API (Very Convenient, not very flexible)
 model = tf.keras.Sequential(
     [
-        tf.keras.layers.InputLayer(input_shape=(28, 28),
+        tf.keras.layers.InputLayer(input_shape=(28*28),
                                    batch_size=1),
         CustomConvolutionLayer(3),
         CustomConvolutionLayer(3),
-        layers.Flatten(),
         layers.Dense(512, activation="relu"),
         layers.Dense(256, activation="relu"),
         layers.Dense(10)
--- main.py
+++ main.py
@@ -10,50 +10,62 @@ tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
 def convolution_2d(inputs, filter):
-    input_shape = inputs.shape
-    batch_size = input_shape[0]
-    filter_size = filter.shape[0]
-
-    output_shape = input_shape[1] - filter_size + 1
-
-    # iterate through data
-    output_temp = []
-    for c in range(batch_size):
-        # iterate through each pixel/data point
-        temp = []
-        for i in range(output_shape):
-            row = tf.TensorArray(tf.float32, size=output_shape, dynamic_size=False, clear_after_read=False)
-            for j in range(output_shape):
-                element = tf.constant(0, dtype=tf.float32)
-                for u in range(filter_size):
-                    for v in range(filter_size):
-                        n = tf.multiply(filter[u][v], inputs[c][i + u][j + v])
-                        element = tf.add(element, n)
-
-                row = row.write(j, element)
-            row = row.stack()
-            temp.append(row)
-
-        output_temp.append(tf.concat([temp], 0))
-    output = tf.concat([output_temp], 0)
-    return output
+    return inputs
 
 
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-x_train = x_train.reshape(-1, 28 * 28).astype("float32")/255.0
-x_test = x_test.reshape(-1, 28 * 28).astype("float32")/255.0
-print(x_train.shape)
+# (x_train, y_train), (x_test, y_test) = mnist.load_data()
+# x_train = x_train.reshape(-1, 28 * 28).astype("float32")/255.0
+# x_test = x_test.reshape(-1, 28 * 28).astype("float32")/255.0
+# print(x_train.shape)
+#
+# A = tf.constant([[[3, 0, 1, 2, 7, 4],
+#                   [1, 5, 8, 9, 3, 1],
+#                   [2, 7, 2, 5, 1, 3],
+#                   [0, 1, 3, 1, 7, 8],
+#                   [4, 2, 1, 6, 2, 8],
+#                   [2, 4, 5, 2, 3, 9]]], dtype=tf.float32)
+# B = tf.concat([A, A], 0)
+#
+# f = tf.constant([[1, 0, -1],
+#                  [1, 0, -1],
+#                  [1, 0, -1]], dtype=tf.float32)
+#
+# out = convolution_2d(B, f)
 
-A = tf.constant([[[3, 0, 1, 2, 7, 4],
-                  [1, 5, 8, 9, 3, 1],
-                  [2, 7, 2, 5, 1, 3],
-                  [0, 1, 3, 1, 7, 8],
-                  [4, 2, 1, 6, 2, 8],
-                  [2, 4, 5, 2, 3, 9]]], dtype=tf.float32)
-B = tf.concat([A, A], 0)
+# toeplitz diagonal
+input_shape = 6
+kernel_shape = 3
+output_shape = input_shape - kernel_shape + 1
+kernel = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
 
-f = tf.constant([[1, 0, -1],
-                 [1, 0, -1],
-                 [1, 0, -1]], dtype=tf.float32)
+row = tf.TensorArray(tf.float32, size=input_shape * input_shape, dynamic_size=False, clear_after_read=False)
+zeros = tf.TensorArray(tf.float32, size=input_shape * input_shape, dynamic_size=False, clear_after_read=False)
 
-out = convolution_2d(B, f)
+for i in range(input_shape * input_shape):
+    if i % input_shape < kernel_shape and i // input_shape < kernel_shape:
+        row = row.write(i, kernel[i // input_shape][(i % input_shape) % kernel_shape])
+    else:
+        row = row.write(i, 0)
+    if i == 0:
+        zeros = zeros.write(i, kernel[0][0])
+    else:
+        zeros = zeros.write(i, 0)
+
+row = row.stack()
+zeros = zeros.stack()
+
+out = tf.linalg.LinearOperatorToeplitz(zeros, row).to_dense()
+
+temp = []
+for i in range(input_shape ** 2):
+    j = 0
+    if not (i % input_shape > input_shape - kernel_shape):
+        temp.append(out[i])
+
+    if len(temp) >= output_shape ** 2:
+        break
+
+out2 = tf.concat([temp], 0)
+
+tf.print("out2")
+tf.print(out2, summarize=36)
