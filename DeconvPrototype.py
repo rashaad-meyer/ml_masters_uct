09d6e0fdf04f72759e3d19b03de039150a4a8f02
viibrem laptop <rashaad.meyer@gmail.com>
2022-04-19 00:23:35 +0200

    Deconvolution algorithm

---
+++ DeconvPrototype.py
@@ -0,0 +1,23 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+
+def forward_fcn(x, h):
+    y = []
+    for i in range(len(x)):
+        y.append(0)
+        if i >= len(h):
+            for j in range(len(h)):
+                y[i] = y[i] + h[j]*y[i-j-1]
+            y[i] = y[i] + x[i]
+    return y
+
+
+if __name__ == '__main__':
+    x_ = [1, 2, 3, 4, 5, 6]
+    h_ = [1, 2, 3]
+    y_ = forward_fcn(x_, h_)
+    print(x_)
+    print(h_)
+    print(y_)
