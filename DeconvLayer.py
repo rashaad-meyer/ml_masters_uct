4e02bd4ee59482769a1670a0d59955c350f34036
viibrem <rashaad.meyer@gmail.com>
2022-04-19 23:18:23 +0200

    Implemented deconvolution layer

--- DeconvLayer.py
+++ DeconvLayer.py
@@ -15,6 +15,24 @@ class DeconvLayer(layers.Layer):
         )
 
     def call(self, inputs):
-        return self.custom_op(inputs)
+        out = []
+        for i in inputs:
+            out.append(self.forward_fcn(inputs, self.w))
+        out = tf.concat([out], 0)
+        return out
 
-
+    @staticmethod
+    def forward_fcn(x, h):
+        y = tf.TensorArray(tf.float32, size=x.shape[-1], dynamic_size=False, clear_after_read=False)
+        v = []
+        for i in range(x.shape[-1]):
+            element = tf.constant(0, dtype=tf.float32)
+            if i >= h.shape[-1]:
+                for j in range(h.shape[-1]):
+                    temp = tf.multiply(h[j], v[i - j - 1])
+                    element = tf.add(element, temp)
+                element = tf.add(element, x[i])
+            v.append(element)
+            y = y.write(i, element)
+        y = y.stack()
+        return y
--- DeconvPrototype.py
+++ DeconvPrototype.py
@@ -4,21 +4,32 @@ from tensorflow.keras import layers
 
 
 def forward_fcn(x, h):
-    # TODO change to use tensors
     y = tf.TensorArray(tf.float32, size=x.shape[-1], dynamic_size=False, clear_after_read=False)
-    for i in range(len(x)):
-        y.append(0)
-        if i >= len(h):
-            for j in range(len(h)):
-                y[i] = y[i] + h[j]*y[i-j-1]
-            y[i] = y[i] + x[i]
+    v = []
+    for i in range(x.shape[-1]):
+        element = tf.constant(0, dtype=tf.float32)
+        if i >= h.shape[-1]:
+            for j in range(h.shape[-1]):
+                temp = tf.multiply(h[j], v[i-j-1])
+                element = tf.add(element, temp)
+            element = tf.add(element, x[i])
+        v.append(element)
+        y = y.write(i, element)
+    y = y.stack()
     return y
 
 
 if __name__ == '__main__':
-    x_ = [1, 2, 3, 4, 5, 6]
-    h_ = [1, 2, 3]
+    # x_ = [1, 2, 3, 4, 5, 6]
+    # h_ = [1, 2, 3]
+    x_ = tf.constant([1, 2, 3, 4, 5, 6], dtype=tf.float32)
+    h_ = tf.constant([1, 2, 3], dtype=tf.float32)
+
     y_ = forward_fcn(x_, h_)
     print(x_)
     print(h_)
     print(y_)
+
+    # for i in range(x_.shape[-1] - h_.shape[-1]):
+    #     print(x_[1:3, :-1])
+
