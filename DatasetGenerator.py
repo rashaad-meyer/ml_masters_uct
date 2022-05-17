9b76dd4e2b99266d63e5d27cfe66112cc8c6ae9c
viibrem <rashaad.meyer@gmail.com>
2022-05-17 12:41:15 +0200

    Fixed code errors

--- DatasetGenerator.py
+++ DatasetGenerator.py
@@ -4,28 +4,17 @@ import DeconvPrototype
 
 def generate_deconv_dataset(h, input_shape):
     x = tf.random.uniform(shape=input_shape, minval=0)
-    y = forward(x, h)
+    hr = tf.reverse(h, [1])
+
+    y = DeconvPrototype.full_deconv(x, h, hr)
     return x, y
-
-
-def forward(inputs, h):
-    v = []
-    for i in inputs:
-        v.append(DeconvPrototype.forward_fcn(i, h))
-    v = tf.concat([v], 0)
-
-    y = []
-    for i in v:
-        y.append(DeconvPrototype.forward_fcn(i, h))
-    y = tf.concat([y], 0)
-
-    return y
 
 
 if __name__ == '__main__':
     h = tf.random.uniform(shape=[1, 8], minval=0)
+    tf.print(h, summarize=8)
     input_shape = [10, 32]
     tf.print(h)
     (x_train, y_train) = generate_deconv_dataset(h, input_shape)
-    tf.print(x_train)
-    tf.print(y_train)
+    tf.print(x_train.shape)
+    tf.print(y_train.shape)
