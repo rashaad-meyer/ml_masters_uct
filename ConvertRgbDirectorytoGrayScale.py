dc532de5282b49959ec4d818ec21c56270661eea
viibrem <rashaad.meyer@gmail.com>
2022-06-05 23:22:27 +0200

    Fixed error where code would throw exception when folder already existed

--- ConvertRgbDirectorytoGrayScale.py
+++ ConvertRgbDirectorytoGrayScale.py
@@ -5,21 +5,27 @@ from tensorflow.keras import layers
 import matplotlib.image as mpimg
 import matplotlib.pyplot as plt
 
-database_path = 'C:\\Users\\Rashaad\\Documents\\Postgrad\\Datasets\\FlickrMaterialDatabase\\image'
-output_path = 'C:\\Users\\Rashaad\\Documents\\Postgrad\\Datasets\\FlickrMaterialDatabase_grayscale\\image'
+database_path = 'C:\\Users\\Rashaad\\Documents\\Postgrad\\Datasets\\dtd\\images'
+output_path = 'C:\\Users\\Rashaad\\Documents\\Postgrad\\Datasets\\dtd_grayscale\\images'
 
 folders = os.listdir(database_path)
 
 for i in folders:
     img_base_path = os.path.join(database_path, i)
-    img_names = os.listdir(img_base_path)[:100]
+    img_names = os.listdir(img_base_path)
     path_folder = os.path.join(output_path, i)
-    os.makedirs(path_folder)
+    if not os.path.exists(path_folder):
+        os.makedirs(path_folder)
+    print(i)
 
     for j in img_names:
+        if j[-4:] != '.png' and j[-4:] != '.jpg':
+            print(j[-4:])
+            continue
+
         img_path = os.path.join(img_base_path, j)
         img = mpimg.imread(img_path)
-        
+
         if len(img.shape) == 3:
             img_grayscale = tf.image.rgb_to_grayscale(img)
             path_full = os.path.join(path_folder, j)
@@ -28,7 +34,6 @@ for i in folders:
             img = tf.reshape(img, (img.shape[0], img.shape[1], 1))
             path_full = os.path.join(path_folder, j)
             tf.keras.utils.save_img(path_full, img)
-
 
 # Plot first pic of each folder
 # for i in range(len(img_disp)):
