9f32d9be1421f53667bc50a03da44eac0d5d809d
viibrem <rashaad.meyer@gmail.com>
2022-06-06 00:58:33 +0200

    Also resizes images to same size

--- ConvertRgbDirectorytoGrayScale.py
+++ ConvertRgbDirectorytoGrayScale.py
@@ -9,6 +9,7 @@ database_path = 'C:\\Users\\Rashaad\\Documents\\Postgrad\\Datasets\\dtd\\images'
 output_path = 'C:\\Users\\Rashaad\\Documents\\Postgrad\\Datasets\\dtd_grayscale\\images'
 
 folders = os.listdir(database_path)
+img_size = [231, 271]
 
 for i in folders:
     img_base_path = os.path.join(database_path, i)
@@ -29,10 +30,12 @@ for i in folders:
         if len(img.shape) == 3:
             img_grayscale = tf.image.rgb_to_grayscale(img)
             path_full = os.path.join(path_folder, j)
+            img_grayscale = tf.image.resize(img_grayscale, img_size)
             tf.keras.utils.save_img(path_full, img_grayscale)
         else:
             img = tf.reshape(img, (img.shape[0], img.shape[1], 1))
             path_full = os.path.join(path_folder, j)
+            img = tf.image.resize(img, img_size)
             tf.keras.utils.save_img(path_full, img)
 
 # Plot first pic of each folder
