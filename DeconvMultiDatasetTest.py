ecde86345563445eb91276d802a98f3d0276ccec
viibrem <rashaad.meyer@gmail.com>
2022-06-10 16:05:04 +0200

    Separated different tests into different methods also added a train and evaluate method

--- DeconvMultiDatasetTest.py
+++ DeconvMultiDatasetTest.py
@@ -7,8 +7,7 @@ from tensorflow.keras.datasets import cifar10
 from DeconvDft2dLayer import DeconvDft2dLayer
 
 
-def mnist_dataset_test():
-
+def mnist_deconv_test():
     print('\n=================================================================')
     print('=================================================================')
     print('                       Deconvolutional Model                     ')
@@ -27,15 +26,31 @@ def mnist_dataset_test():
     outputs = layers.Dense(10, activation="softmax")(x)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-    model.summary()
+    train_and_evaluate(model, x_test, x_train, y_test, y_train)
 
-    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-                  optimizer=tf.keras.optimizers.Adam(lr=0.001),
-                  metrics=['accuracy'])
 
-    model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
-    model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+def mnist_conv_test():
+    print('\n=================================================================')
+    print('=================================================================')
+    print('                      Convolution Model                          ')
+    print('=================================================================')
+    print('=================================================================\n')
 
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    x_train = x_train.astype("float32") / 255.0
+    x_test = x_test.astype("float32") / 255.0
+
+    # Functional API for Dense layer
+    inputs = tf.keras.Input(shape=(28, 28, 1))
+    x = layers.Conv2D(1, (3, 3))(inputs)
+    x = layers.Dense(256, activation="relu", name="second_layer")(x)
+    outputs = layers.Dense(10, activation="softmax")(x)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+
+    train_and_evaluate(model, x_test, x_train, y_test, y_train)
+
+
+def mnist_dense_test():
     print('\n=================================================================')
     print('=================================================================')
     print('                         Dense Model                             ')
@@ -53,14 +68,7 @@ def mnist_dataset_test():
     outputs = layers.Dense(10, activation="softmax")(x)
     model = tf.keras.Model(inputs=inputs, outputs=outputs)
 
-    model.summary()
-
-    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
-                  optimizer=tf.keras.optimizers.Adam(lr=0.001),
-                  metrics=['accuracy'])
-
-    model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
-    model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+    train_and_evaluate(model, x_test, x_train, y_test, y_train)
 
 
 def cifar10_dataset_test():
@@ -101,9 +109,26 @@ def cifar10_dataset_test():
     model.evaluate(x_test, y_test, batch_size=64, verbose=2)
 
 
+def train_and_evaluate(model, x_test, x_train, y_test, y_train):
+    model.summary()
+    model.compile(loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+                  optimizer=tf.keras.optimizers.Adam(lr=0.001),
+                  metrics=['accuracy'])
+    history = model.fit(x_train, y_train, batch_size=32, epochs=5, verbose=2)
+    results = model.evaluate(x_test, y_test, batch_size=32, verbose=2)
+
+    return history, results
+
+
+def mnist_test_comparison():
+    mnist_dense_test()
+    mnist_deconv_test()
+    mnist_conv_test()
+
+
 if __name__ == '__main__':
     physical_devices = tf.config.list_physical_devices("GPU")
     tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
     # cifar10_dataset_test()
-    mnist_dataset_test()
+    mnist_test_comparison()
