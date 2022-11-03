73d24dfc92212240a01af570defd54519c9cb278
viibrem <rashaad.meyer@gmail.com>
2022-11-03 18:49:25 +0200

    Added generalisation to custom dense layer // Extracted function for test

--- Deconvolution/LayerTests/CustomGradientTest.py
+++ Deconvolution/LayerTests/CustomGradientTest.py
@@ -24,29 +24,24 @@ def custom_operation(w, b, x):
 
 # end def custom_op
 
-# Generate training data
-W = tf.random.uniform((2, 3))
-x_train = np.array(tf.random.uniform((10, 2)))
-y_train = np.array(tf.matmul(x_train, W))
-y_train = y_train + 0.01 * tf.random.normal(tf.shape(y_train))
-
 
 class CustomLayer(tf.keras.layers.Layer):
-    def __init__(self):
+    def __init__(self, hidden_size):
         super(CustomLayer, self).__init__()
         self.w = None
         self.b = None
+        self.hidden_size = hidden_size
 
     def build(self, input_shape):
         self.w = self.add_weight(
             name='w',
-            shape=(2, 3),
+            shape=(input_shape[1], self.hidden_size),
             initializer='random_normal',
             trainable=True
         )
         self.b = self.add_weight(
             name='b',
-            shape=(3,),
+            shape=(self.hidden_size,),
             initializer='random_normal',
             trainable=True
         )
@@ -55,10 +50,16 @@ class CustomLayer(tf.keras.layers.Layer):
         return custom_operation(self.w, self.b, x)
 
 
-inputs = layers.Input(2)
-outputs = CustomLayer()(inputs)
-model = tf.keras.Model(inputs=inputs, outputs=outputs)
-model.compile(optimizer=tf.keras.optimizers.SGD(),
-              loss=tf.keras.losses.MeanSquaredError())
-model.fit(x_train, y_train, batch_size=5, epochs=5, verbose=2)
+if __name__ == '__main__':
+    # Generate training data
+    W = tf.random.uniform((2, 3))
+    x_train = np.array(tf.random.uniform((10, 2)))
+    y_train = np.array(tf.matmul(x_train, W))
+    y_train = y_train + 0.01 * tf.random.normal(tf.shape(y_train))
 
+    inputs = layers.Input(2)
+    outputs = CustomLayer(3)(inputs)
+    model = tf.keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=tf.keras.optimizers.SGD(),
+                  loss=tf.keras.losses.MeanSquaredError())
+    model.fit(x_train, y_train, batch_size=5, epochs=5, verbose=2)
