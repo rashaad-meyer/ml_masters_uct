e5033bc09f0efe3fb62cdd6288651e2eadd1d1b2
viibrem laptop <rashaad.meyer@gmail.com>
2022-11-22 23:28:51 +0200

    Changed to use real FFTs

--- Deconvolution/LayerTests/DeconvGradientCheck.py
+++ Deconvolution/LayerTests/DeconvGradientCheck.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from Deconvolution.CustomLayers.DeconvDft2dCustomGradLayer import custom_op
 
 
 def check_gradient():
@@ -18,42 +19,7 @@ def check_gradient():
 
     # Calculate forward pass and get gradient tape to keep track of operations
     with tf.GradientTape() as tape:
-        xm = tf.reshape(xm, (-1, xm.shape[-3], xm.shape[-2]))
-
-        pad_w = tf.constant([[0, 0], [1, 0]])
-        # Set first element to 1 then reshape into specified filter shape
-        w0 = tf.pad(w, pad_w, mode='CONSTANT', constant_values=1)
-        w0 = tf.reshape(w0, h_shape)
-
-        padding = tf.constant(
-            [[0, 0], [int(xm.shape[-2] * pad_amount), int(xm.shape[-2] * pad_amount)],
-             [int(xm.shape[-1] * pad_amount), int(xm.shape[-1] * pad_amount)]])
-
-        xm = tf.pad(xm, padding, "CONSTANT")
-        paddings = tf.constant([[0, xm.shape[-2] - w0.shape[-2]], [0, xm.shape[-1] - w0.shape[-1]]])
-
-        hm1 = tf.pad(w0, paddings, "CONSTANT")
-        xm = tf.cast(xm, dtype=tf.complex64)
-        hm1 = tf.cast(hm1, dtype=tf.complex64)
-
-        gm1f = tf.divide(1, tf.signal.fft2d(hm1))
-        gm2f = tf.roll(tf.reverse(gm1f, [0]), shift=1, axis=0)
-        gm3f = tf.roll(tf.reverse(gm1f, [1]), shift=1, axis=1)
-        gm4f = tf.roll(tf.reverse(gm3f, [0]), shift=1, axis=0)
-
-        gmf1 = tf.multiply(gm1f, gm2f)
-        gmf2 = tf.multiply(gm3f, gm4f)
-        gmf = tf.multiply(gmf1, gmf2)
-        ymf = tf.multiply(gmf, tf.signal.fft2d(xm))
-
-        ym = tf.signal.ifft2d(ymf)
-        ym = tf.cast(ym, dtype=tf.float32)
-        ym = tf.reshape(ym, (-1, ym.shape[-2], ym.shape[-1], 1))
-
-        # crop to original image size
-        ym = tf.image.central_crop(ym, 1 / (1 + 2 * pad_amount))
-
-        loss = tf.reduce_mean(ym ** 2)
+        loss = forward_prop(h_shape, pad_amount, w, xm)
 
     # get gradients from GradientTape
     grad = tape.gradient(loss, w)
@@ -74,6 +40,7 @@ def check_gradient():
             "\033[92m" + "Your backward propagation works perfectly fine! difference = " + str(difference) + "\033[0m")
 
 
+@tf.function
 def forward_prop(h_shape, pad_amount, w, xm):
     with tf.GradientTape() as tape:
         xm = tf.reshape(xm, (-1, xm.shape[-3], xm.shape[-2]))
@@ -91,10 +58,8 @@ def forward_prop(h_shape, pad_amount, w, xm):
         paddings = tf.constant([[0, xm.shape[-2] - w0.shape[-2]], [0, xm.shape[-1] - w0.shape[-1]]])
 
         hm1 = tf.pad(w0, paddings, "CONSTANT")
-        xm = tf.cast(xm, dtype=tf.complex64)
-        hm1 = tf.cast(hm1, dtype=tf.complex64)
 
-        gm1f = tf.divide(1, tf.signal.fft2d(hm1))
+        gm1f = tf.divide(1, tf.signal.rfft2d(hm1))
         gm2f = tf.roll(tf.reverse(gm1f, [0]), shift=1, axis=0)
         gm3f = tf.roll(tf.reverse(gm1f, [1]), shift=1, axis=1)
         gm4f = tf.roll(tf.reverse(gm3f, [0]), shift=1, axis=0)
@@ -102,10 +67,9 @@ def forward_prop(h_shape, pad_amount, w, xm):
         gmf1 = tf.multiply(gm1f, gm2f)
         gmf2 = tf.multiply(gm3f, gm4f)
         gmf = tf.multiply(gmf1, gmf2)
-        ymf = tf.multiply(gmf, tf.signal.fft2d(xm))
+        ymf = tf.multiply(gmf, tf.signal.rfft2d(xm))
 
-        ym = tf.signal.ifft2d(ymf)
-        ym = tf.cast(ym, dtype=tf.float32)
+        ym = tf.signal.irfft2d(ymf)
         ym = tf.reshape(ym, (-1, ym.shape[-2], ym.shape[-1], 1))
 
         # crop to original image size
