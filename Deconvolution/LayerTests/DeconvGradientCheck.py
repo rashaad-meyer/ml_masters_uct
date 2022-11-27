4c0faefb00e52e8fc35aaa059cd820de6bed2ada
viibrem laptop <rashaad.meyer@gmail.com>
2022-11-27 22:03:26 +0200

    Implemented custom grad check

--- Deconvolution/LayerTests/DeconvGradientCheck.py
+++ Deconvolution/LayerTests/DeconvGradientCheck.py
@@ -107,8 +107,136 @@ def get_grad_approx(h_shape, pad_amount, w, xm, eps=1e-7):
 def custom_grad_check():
     # Expected output:
     # 102.2888  113.4538  115.5920  125.2290  118.3843   74.5657   98.5300
-    return
+    h_shape = (2, 4)
+
+    X = tf.range(0, h_shape[0])
+    Y = tf.range(0, h_shape[1])
+    X1, X2 = tf.meshgrid(X, Y)
+    X1 = tf.reshape(X1, (1, -1))
+    X2 = tf.reshape(X2, (1, -1))
+
+    hsir = {}
+
+    hsirf = tf.concat([X1, X2], axis=0)
+
+    hsir['G1'] = hsirf[:, 1:]
+    hsir['G2'] = tf.concat([-X1, X2], axis=0)[:, 1:]
+    hsir['G3'] = tf.concat([X1, -X2], axis=0)[:, 1:]
+    hsir['G4'] = tf.concat([-X1, -X2], axis=0)[:, 1:]
+
+    tf.random.set_seed(42)
+
+    w = tf.constant([[1, 0.1, 0.2, -0.1],
+                  [0, 0.1, 0.3, 0]])
+
+
+    xm = tf.random.uniform((1, 28, 28), seed=42)
+    um = tf.random.uniform((1, 28, 28))
+
+    paddings = tf.constant([[0, xm.shape[-2] - w.shape[-2]],
+                            [0, xm.shape[-1] - w.shape[-1]]])
+    hm1 = tf.pad(w, paddings, "CONSTANT")
+
+    xm = tf.cast(xm, dtype=tf.complex64)
+    hm1 = tf.cast(hm1, dtype=tf.complex64)
+
+    gm1f = tf.divide(1, tf.signal.fft2d(hm1))
+    gm2f = tf.roll(tf.reverse(gm1f, [0]), shift=1, axis=0)
+    gm3f = tf.roll(tf.reverse(gm1f, [1]), shift=1, axis=1)
+    gm4f = tf.roll(tf.reverse(gm3f, [0]), shift=1, axis=0)
+
+    gmf1 = tf.multiply(gm1f, gm2f)
+    gmf2 = tf.multiply(gm3f, gm4f)
+    gmf = tf.multiply(gmf1, gmf2)
+
+    ymf = tf.multiply(gmf, tf.signal.fft2d(xm))
+    ym = tf.signal.ifft2d(ymf)
+    ym = tf.cast(ym, dtype=tf.float32)
+
+    def grad_fn(um):
+        # um = tf.reshape(um, (-1, um.shape[-3], um.shape[-2]))
+        umi = tf.cast(um, dtype=tf.complex64)
+
+        # backprop layer inputs
+
+        umf = tf.signal.fft2d(umi)
+        dldxf = tf.multiply(gmf, umf)
+        dldx = tf.signal.ifft2d(dldxf)
+        dldx = tf.cast(dldx, dtype=tf.float32)
+
+        dldw = tf.zeros((1, hsir['G1'].shape[1]))
+
+        vm = tf.signal.ifft2d(tf.multiply(gm1f, ymf))
+        vm = tf.cast(vm, tf.float32)
+
+        # G1
+        hsirf = hsir['G1']
+
+        vm = tf.signal.ifft2d(tf.multiply(gm1f, ymf))
+        vm = tf.cast(vm, tf.float32)
+
+        for j in range(hsirf.shape[1]):
+            j_zeros = np.zeros((1, hsirf.shape[1]), dtype=np.float32)
+            j_zeros[0][j] = 1
+            shift = tf.transpose(hsirf[:, j])
+            vmq = tf.roll(vm, shift=shift, axis=[1, 2])
+            temp = tf.multiply(vmq, um)
+            temp = tf.multiply(j_zeros, tf.reduce_sum(temp))
+            dldw = tf.add(dldw, temp)
+
+        # G2
+        hsirf = hsir['G2']
+
+        vm = tf.signal.ifft2d(tf.multiply(gm2f, ymf))
+        vm = tf.cast(vm, tf.float32)
+
+        for j in range(hsirf.shape[1]):
+            j_zeros = np.zeros((1, hsirf.shape[1]), dtype=np.float32)
+            j_zeros[0][j] = 1
+            shift = tf.transpose(hsirf[:, j])
+            vmq = tf.roll(vm, shift=shift, axis=[1, 2])
+            temp = tf.multiply(vmq, um)
+            temp = tf.multiply(j_zeros, tf.reduce_sum(temp))
+            dldw = tf.add(dldw, temp)
+
+        # G3
+        hsirf = hsir['G3']
+
+        vm = tf.signal.ifft2d(tf.multiply(gm3f, ymf))
+        vm = tf.cast(vm, tf.float32)
+
+        for j in range(hsirf.shape[1]):
+            j_zeros = np.zeros((1, hsirf.shape[1]), dtype=np.float32)
+            j_zeros[0][j] = 1
+            shift = tf.transpose(hsirf[:, j])
+            vmq = tf.roll(vm, shift=shift, axis=[1, 2])
+            temp = tf.multiply(vmq, um)
+            temp = tf.multiply(j_zeros, tf.reduce_sum(temp))
+            dldw = tf.add(dldw, temp)
+
+        # G4
+        hsirf = hsir['G4']
+
+        vm = tf.signal.ifft2d(tf.multiply(gm4f, ymf))
+        vm = tf.cast(vm, tf.float32)
+
+        for j in range(hsirf.shape[1]):
+            j_zeros = np.zeros((1, hsirf.shape[1]), dtype=np.float32)
+            j_zeros[0][j] = 1
+            shift = tf.transpose(hsirf[:, j])
+            vmq = tf.roll(vm, shift=shift, axis=[1, 2])
+            temp = tf.multiply(vmq, um)
+            temp = tf.multiply(j_zeros, tf.reduce_sum(temp))
+            dldw = tf.add(dldw, temp)
+
+        return dldx, dldw, None, None, None, None, None, None
+
+    grads = grad_fn(um)
+
+    return grads[1]
 
 
 if __name__ == '__main__':
-    check_gradient()
+    # check_gradient()
+    dldw = custom_grad_check()
+    print(dldw)
