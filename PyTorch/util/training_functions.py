1b7e93a13e0fbbab8593f22513f82da6544c1e2b
viibrem laptop <rashaad.meyer@gmail.com>
2023-02-16 19:25:00 +0200

    Added classification training function

---
+++ PyTorch/util/training_functions.py
@@ -0,0 +1,50 @@
+import torch
+import torch.nn as nn
+import tqdm
+
+def train_classification_model(model: nn.Module, criterion, optimizer, dataloader, num_epochs=3):
+    """
+    Trains NN classifier on classification dataset
+    :param model: The NN model that you would like to train must be of type nn.Module
+    :param criterion: The loss function that you would like to use
+    :param optimizer: The optimizer that will optimize the NN
+    :param dataloader: Dataloader that loads data from classification dataset
+    :param num_epochs: Number of times you want to train the data over
+    :return: A dictionary containing training loss and accuracy for each epoch
+    """
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    history = {'loss': [], 'accuracy': []}
+    for epoch in range(num_epochs):
+        print(f'Epoch {epoch + 1:2d}/{num_epochs}')
+
+        model.train()
+        running_loss = 0.0
+        running_correct = 0
+        data_len = 0
+
+        for X, labels in tqdm(dataloader):
+            X = X.to(device)
+            labels = labels.to(device)
+
+            with torch.set_grad_enabled(True):
+                outputs = model(X)
+                loss = criterion(outputs, labels)
+                _, preds = torch.max(outputs, 1)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            running_loss += loss
+            running_correct += torch.sum(preds == labels.data)
+            data_len += X.size(0)
+
+        epoch_loss = running_loss
+        epoch_acc = running_correct / data_len
+
+        history['loss'].append(epoch_loss.item())
+        history['accuracy'].append(epoch_acc.item())
+        print('Loss: {:.4f}, Acc: {:.3f}'.format(epoch_loss, epoch_acc))
+
+    return history
