ce495b3b8b53745a8224e7ff388edcb653bffed4
viibrem laptop <rashaad.meyer@gmail.com>
2023-02-20 16:31:02 +0200

    Added regression model training function

--- PyTorch/util/training_functions.py
+++ PyTorch/util/training_functions.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import tqdm
 
+
 def train_classification_model(model: nn.Module, criterion, optimizer, dataloader, num_epochs=3):
     """
     Trains NN classifier on classification dataset
@@ -46,5 +47,44 @@ def train_classification_model(model: nn.Module, criterion, optimizer, dataloade
         history['loss'].append(epoch_loss.item())
         history['accuracy'].append(epoch_acc.item())
         print('Loss: {:.4f}, Acc: {:.3f}'.format(epoch_loss, epoch_acc))
+
+    return history
+
+
+def train_regression_model(model: nn.Module, criterion, optimizer, dataloader, num_epochs=3):
+    """
+        Trains NN regression on datasets for deblurring and super image resolution
+        :param model: The NN model that you would like to train must be of type nn.Module
+        :param criterion: The loss function that you would like to use
+        :param optimizer: The optimizer that will optimize the NN
+        :param dataloader: Dataloader that loads data from classification dataset
+        :param num_epochs: Number of times you want to train the data over
+        :return: A dictionary containing training loss for each epoch
+    """
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
+    history = {'loss': [], 'accuracy': []}
+    for epoch in range(num_epochs):
+        model.train()
+        running_loss = 0.0
+
+        for X, y in dataloader:
+            X = X.to(device)
+            y = y.to(device)
+
+            with torch.set_grad_enabled(True):
+                outputs = model(X)
+                loss = criterion(outputs, y)
+
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            running_loss += loss
+
+        history['loss'].append(running_loss)
+
+        if (epoch + 1) % 50 == 0:
+            print('Epoch {:04d} loss: {:.5f}'.format(epoch + 1, running_loss))
 
     return history
