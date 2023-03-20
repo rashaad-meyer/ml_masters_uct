62e08716b0e62ae1a88c9738bb927aec0f24f4af
Rashaad <rashaad.meyer@gmail.com>
2023-03-20 15:08:50 +0200

    Added a class that implemented multiple filters for deconv layer

--- PyTorch/Models/DeconvModels.py
+++ PyTorch/Models/DeconvModels.py
@@ -46,3 +46,41 @@ class Deconv2DWithScaling(nn.Module):
         x = self.deconv(x)
         x = self.w * x
         return x
+
+
+class Deconv2DMultiFilter(nn.Module):
+    def __init__(self, shape=(2, 4), filters=1):
+        super(Deconv2DMultiFilter, self).__init__()
+
+        temp = torch.rand((filters,) + shape)
+        temp_sum = torch.sum(temp, dim=(1, 2))
+
+        temp = temp / temp_sum.view((filters, 1, 1))
+
+        temp[:, 0, 0] = 1.0
+
+        self.w = nn.Parameter(data=temp, requires_grad=True)
+
+    def forward(self, x):
+
+        w = self.w
+        hm1 = nn.functional.pad(w, (0, x.size(-1) - w.size(-1), 0, x.size(-2) - w.size(-2)))
+
+        gm1f = 1 / fft2(hm1)
+
+        gm2f_ = torch.flip(gm1f, (0,))
+        gm2f = torch.roll(gm2f_, shifts=1, dims=0)
+
+        gm3f_ = torch.flip(gm1f, (1,))
+        gm3f = torch.roll(gm3f_, shifts=1, dims=1)
+
+        gm4f_ = torch.flip(gm1f, (0, 1))
+        gm4f = torch.roll(gm4f_, shifts=(1, 1), dims=(0, 1))
+
+        gmf = gm1f * gm2f * gm3f * gm4f
+
+        ymf = gmf * fft2(x)
+
+        y = ifft2(ymf).real
+
+        return y
