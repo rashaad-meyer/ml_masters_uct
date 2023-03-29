2498c4c2d2f4f5b982b12b8b8bd5f54663cd0c6e
Rashaad <rashaad.meyer@gmail.com>
2023-03-29 10:53:21 +0200

    Added ssim and ms_ssim code from https://github.com/VainF/pytorch-msssim/tree/master

--- PyTorch/Models/LossModules.py
+++ PyTorch/Models/LossModules.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import numpy as np
+from PyTorch.util.ssim import ssim, ms_ssim, fspecial_gauss_1d
 
 
 class DCT(nn.Module):
@@ -50,9 +51,9 @@ class DCT(nn.Module):
         return y
 
 
-class MseWithDct(nn.Module):
+class MSE_WITH_DCT(nn.Module):
     def __init__(self, img_size=(96, 96)):
-        super(MseWithDct, self).__init__()
+        super(MSE_WITH_DCT, self).__init__()
         self.mse = nn.MSELoss()
         self.dct = DCT(img_size)
 
@@ -61,3 +62,89 @@ class MseWithDct(nn.Module):
         y = self.dct(y)
         loss = self.mse(x, y)
         return loss
+
+
+class SSIM(torch.nn.Module):
+    def __init__(
+            self,
+            data_range=255,
+            size_average=True,
+            win_size=11,
+            win_sigma=1.5,
+            channel=3,
+            spatial_dims=2,
+            K=(0.01, 0.03),
+            nonnegative_ssim=False,
+    ):
+        r""" class for ssim
+        Args:
+            data_range (float or int, optional): value range of input images. (usually 1.0 or 255)
+            size_average (bool, optional): if size_average=True, ssim of all images will be averaged as a scalar
+            win_size: (int, optional): the size of gauss kernel
+            win_sigma: (float, optional): sigma of normal distribution
+            channel (int, optional): input channels (default: 3)
+            K (list or tuple, optional): scalar constants (K1, K2). Try a larger K2 constant (e.g. 0.4) if you get a negative or NaN results.
+            nonnegative_ssim (bool, optional): force the ssim response to be nonnegative with relu.
+        """
+
+        super(SSIM, self).__init__()
+        self.win_size = win_size
+        self.win = fspecial_gauss_1d(win_size, win_sigma).repeat([channel, 1] + [1] * spatial_dims)
+        self.size_average = size_average
+        self.data_range = data_range
+        self.K = K
+        self.nonnegative_ssim = nonnegative_ssim
+
+    def forward(self, X, Y):
+        return ssim(
+            X,
+            Y,
+            data_range=self.data_range,
+            size_average=self.size_average,
+            win=self.win,
+            K=self.K,
+            nonnegative_ssim=self.nonnegative_ssim,
+        )
+
+
+class MS_SSIM(torch.nn.Module):
+    def __init__(
+            self,
+            data_range=255,
+            size_average=True,
+            win_size=11,
+            win_sigma=1.5,
+            channel=3,
+            spatial_dims=2,
+            weights=None,
+            K=(0.01, 0.03),
+    ):
+        r""" class for ms-ssim
+        Args:
+            data_range (float or int, optional): value range of input images. (usually 1.0 or 255)
+            size_average (bool, optional): if size_average=True, ssim of all images will be averaged as a scalar
+            win_size: (int, optional): the size of gauss kernel
+            win_sigma: (float, optional): sigma of normal distribution
+            channel (int, optional): input channels (default: 3)
+            weights (list, optional): weights for different levels
+            K (list or tuple, optional): scalar constants (K1, K2). Try a larger K2 constant (e.g. 0.4) if you get a negative or NaN results.
+        """
+
+        super(MS_SSIM, self).__init__()
+        self.win_size = win_size
+        self.win = fspecial_gauss_1d(win_size, win_sigma).repeat([channel, 1] + [1] * spatial_dims)
+        self.size_average = size_average
+        self.data_range = data_range
+        self.weights = weights
+        self.K = K
+
+    def forward(self, X, Y):
+        return ms_ssim(
+            X,
+            Y,
+            data_range=self.data_range,
+            size_average=self.size_average,
+            win=self.win,
+            weights=self.weights,
+            K=self.K,
+        )
