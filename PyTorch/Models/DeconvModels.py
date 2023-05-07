b29c8ca921527d5c281be29436fb7f79486b8fbf
Rashaad <rashaad.meyer@gmail.com>
2023-05-07 11:08:36 +0200

    added flag to set first element to trainable

--- PyTorch/Models/DeconvModels.py
+++ PyTorch/Models/DeconvModels.py
@@ -53,17 +53,18 @@ class Deconv2DWithScaling(nn.Module):
 
 
 class Deconv2DMultiFilter(nn.Module):
-    def __init__(self, in_channels=1, out_channels=1, kernel_size=(2, 4), bias=True):
+    def __init__(self, in_channels=1, out_channels=1, kernel_size=(2, 4), bias=True, first_elem_trainable=False):
         super(Deconv2DMultiFilter, self).__init__()
 
-        # initialise filter weights
-        w = torch.rand((out_channels, in_channels,) + (kernel_size[0] * kernel_size[1] - 1,))
+        if first_elem_trainable:
+            # initialise filter as correct shape so that first element is trainable
+            w = torch.rand((out_channels, in_channels,) + kernel_size)
+        else:
+            # initialise filter as flat to be reshaped after so that first element is not trainable
+            w = torch.rand((out_channels, in_channels,) + (kernel_size[0] * kernel_size[1] - 1,))
 
-        # divide each filter by the sum of their weights multiplied by number of input channels so that
-        # out_channel filters don't increase in
         init_factor = (kernel_size[0] * kernel_size[1] * (in_channels + out_channels))
         w = w / init_factor
-
         w = nn.Parameter(data=w, requires_grad=True)
 
         # make first element of each filter 1.0
@@ -72,6 +73,7 @@ class Deconv2DMultiFilter(nn.Module):
         self.out_channels = out_channels
         self.kernel_size = kernel_size
         self.w = w
+        self.first_elem_trainable = first_elem_trainable
 
         if bias:
             self.b = nn.Parameter(data=torch.rand(1, out_channels, 1, 1) - 0.5, requires_grad=True)
@@ -83,8 +85,9 @@ class Deconv2DMultiFilter(nn.Module):
         x = x.unsqueeze(dim=1)
 
         w = self.w
-        w = nn.functional.pad(w, (1, 0), value=1)
-        w = torch.reshape(w, (self.out_channels, self.in_channels,) + self.kernel_size)
+        if not self.first_elem_trainable:
+            w = nn.functional.pad(w, (1, 0), value=1)
+            w = torch.reshape(w, (self.out_channels, self.in_channels,) + self.kernel_size)
 
         hm1 = nn.functional.pad(w, (0, x.size(-1) - w.size(-1), 0, x.size(-2) - w.size(-2)))
 
