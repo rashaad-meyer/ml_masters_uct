ac3f161d647435aa08575ce3d4034bd740ff9b03
viibrem laptop <rashaad.meyer@gmail.com>
2023-05-09 22:10:22 +0200

    Changed to use nn.Module instead of nn.Sequential due to dimension errors

--- PyTorch/Models/CnnModules.py
+++ PyTorch/Models/CnnModules.py
@@ -2,7 +2,7 @@ import torch.nn as nn
 from PyTorch.Models.DeconvModels import Deconv2DMultiFilter
 
 
-class TwoLayerCNN(nn.Sequential):
+class TwoLayerCNN(nn.Module):
     def __init__(self, img_size=(32, 32), layer_1='conv', layer_2='conv', layer_1_in=3, layer_1_out=32, layer_2_out=32,
                  num_classes=100, deconv_bias=True, first_elem_trainable=True):
         super(TwoLayerCNN, self).__init__()
@@ -21,16 +21,15 @@ class TwoLayerCNN(nn.Sequential):
 
         self.fc1 = nn.Linear(layer_2_out * img_size[0] * img_size[1], num_classes)
 
-        # Add layers to the sequential container
-        self.add_module(f'{layer_1}1', self.layer1)
-        self.add_module('relu1', nn.ReLU())
-
-        self.add_module(f'{layer_2}2', self.layer2)
-        self.add_module('relu2', nn.ReLU())
-
-        self.add_module('flatten', nn.Flatten())
-
-        self.add_module('fc1', self.fc1)
+        self.relu = nn.ReLU()
+        self.flatten = nn.Flatten()
 
     def forward(self, x):
-        return super(TwoLayerCNN, self).forward(x)
+        x = self.layer1(x)
+        x = self.relu(x)
+        x = self.layer2(x)
+        x = self.relu(x)
+        x = self.flatten(x)
+        print(x.size())
+        x = self.fc1(x)
+        return x
