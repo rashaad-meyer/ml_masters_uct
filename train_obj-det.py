6b8d634e56441cb8e4ead3cbacf62ccf03f3f135
Rashaad <rashaad.meyer@gmail.com>
2023-06-17 22:44:16 +0200

    Can now run obj detection TwoLayerCNN and multiple experiments // Implemented wandb for object detection

--- train_obj-det.py
+++ train_obj-det.py
@@ -8,8 +8,11 @@ import torch
 import torchvision.transforms as transforms
 import torch.optim as optim
 import torchvision.transforms.functional as FT
+import wandb
 from tqdm import tqdm
 from torch.utils.data import DataLoader
+
+from PyTorch.Models.CnnModules import TwoLayerCNN
 from PyTorch.ObjectDetection.model import Yolov1
 from PyTorch.ObjectDetection.dataset import VOCDataset
 from PyTorch.ObjectDetection.utils import (
@@ -25,20 +28,15 @@ from PyTorch.ObjectDetection.utils import (
 import torch.utils.data as data_utils
 from PyTorch.ObjectDetection.loss import YoloLoss
 from PyTorch.util.helper_functions import get_voc_ds
+from PyTorch.util.training_functions import compute_impulse_diffs
+from train_classification import read_json_objects
 
 seed = 123
 torch.manual_seed(seed)
 
-# Hyperparameters etc.
-LEARNING_RATE = 2e-5
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 32  # 64 in original paper, but I don't have that much vram, grad accum?
-WEIGHT_DECAY = 0
-EPOCHS = 100
+IMG_SIZE = (448, 448)
 NUM_WORKERS = 2
 PIN_MEMORY = True
-LOAD_MODEL = False
-LOAD_MODEL_FILE = "overfit.pth.tar"
 
 BASE_DIR = "data/obj-det"
 IMG_DIR = f"{BASE_DIR}/images"
@@ -56,26 +54,34 @@ class Compose(object):
         return img, bboxes
 
 
-transform = Compose([transforms.Resize((448, 448)), transforms.ToTensor(), ])
+transform = Compose([transforms.Resize(IMG_SIZE), transforms.ToTensor(), ])
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--ds_len", default=-1, type=int, help="Length of dataset")
+    parser.add_argument("--batch_size", default=64, type=int, help="Batch size for training")
+    parser.add_argument("--epochs", default=100, type=int, help="Number of epochs for training")
+    parser.add_argument("--learning_rate", default=2e-5, type=float, help="Learning rate")
+    parser.add_argument("--weight_decay", default=0, type=float, help="Weight decay for the optimizer")
+    parser.add_argument('--multi', dest='multi', action='store_true')
 
     args = parser.parse_args()
     return args
 
 
 def train_fn(train_loader, model, optimizer, loss_fn):
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    model = model.to(DEVICE)
+
     loop = tqdm(train_loader, leave=True)
-    mean_loss = []
+    losses = []
 
     for batch_idx, (x, y) in enumerate(loop):
         x, y = x.to(DEVICE), y.to(DEVICE)
         out = model(x)
         loss = loss_fn(out, y)
-        mean_loss.append(loss.item())
+        losses.append(loss.item())
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
@@ -83,21 +89,16 @@ def train_fn(train_loader, model, optimizer, loss_fn):
         # update progress bar
         loop.set_postfix(loss=loss.item())
 
-    print(f"Mean loss was {sum(mean_loss) / len(mean_loss)}")
+    mean_loss = sum(losses) / len(losses)
+    print(f"Mean loss was {mean_loss}")
+
+    return mean_loss
 
 
 def main():
     args = parse_args()
     get_voc_ds(BASE_DIR)
     print('Loading model...')
-    model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
-    optimizer = optim.Adam(
-        model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY
-    )
-    loss_fn = YoloLoss()
-
-    if LOAD_MODEL:
-        load_checkpoint(torch.load(LOAD_MODEL_FILE), model, optimizer)
 
     ds_length = None
     if args.ds_len != -1:
@@ -119,7 +120,7 @@ def main():
 
     train_loader = DataLoader(
         dataset=train_dataset,
-        batch_size=BATCH_SIZE,
+        batch_size=args.batch_size,
         num_workers=NUM_WORKERS,
         pin_memory=PIN_MEMORY,
         shuffle=True,
@@ -128,17 +129,52 @@ def main():
 
     test_loader = DataLoader(
         dataset=test_dataset,
-        batch_size=BATCH_SIZE,
+        batch_size=args.batch_size,
         num_workers=NUM_WORKERS,
         pin_memory=PIN_MEMORY,
         shuffle=True,
         drop_last=True,
     )
+    if args.multi:
+        wandb.login()
 
-    print('Staring training...')
-    for epoch in range(EPOCHS):
-        print(f'Epoch {epoch + 1: 5d}/{EPOCHS}:')
-        train_fn(train_loader, model, optimizer, loss_fn)
+        configs = read_json_objects('experiment_csv/obj-det.txt')
+
+        for hyperparams in configs:
+            with wandb.init(project="YoloV1", config=hyperparams):
+                config = wandb.config
+
+                S, B, C = 7, 2, 20
+                num_classes = S * S * (C + B * 5)
+
+                model = TwoLayerCNN(**config, num_classes=num_classes, img_size=IMG_SIZE)
+                optimizer = optim.Adam(
+                    model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay
+                )
+                loss_fn = YoloLoss()
+
+                print('Starting training...')
+                train_yolo(args, loss_fn, model, optimizer, train_loader)
+
+    else:
+        DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+        model = Yolov1(split_size=7, num_boxes=2, num_classes=20).to(DEVICE)
+        optimizer = optim.Adam(
+            model.parameters(), lr=args.learning_rate, weight_decay=args.weight_decay
+        )
+        loss_fn = YoloLoss()
+
+        print('Starting training...')
+        train_yolo(args, loss_fn, model, optimizer, train_loader)
+
+
+def train_yolo(args, loss_fn, model, optimizer, train_loader):
+    wandb.watch(model, loss_fn, log="all", log_freq=10)
+    compute_impulse_diffs(train_loader, 0, model)
+
+    for epoch in range(args.epochs):
+        print(f'Epoch {epoch + 1: 5d}/{args.epochs}:')
+        loss = train_fn(train_loader, model, optimizer, loss_fn)
 
         print('Computing mAP...')
         pred_boxes, target_boxes = get_bboxes(
@@ -147,6 +183,9 @@ def main():
         mean_avg_prec = mean_average_precision(
             pred_boxes, target_boxes, iou_threshold=0.5, box_format="midpoint"
         )
+        wandb.log({"epoch_loss": loss, "mAP": mean_avg_prec}, step=epoch)
+        compute_impulse_diffs(train_loader, epoch + 1, model)
+
         print(f"Train mAP: {mean_avg_prec}")
         print('==================================================================\n')
 
