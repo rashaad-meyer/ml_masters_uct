8883c3979fe6020480bd1af39c31ea73f598535f
Rashaad <rashaad.meyer@gmail.com>
2023-11-02 16:13:52 +0200

    loading state dict not strict to bypass error

--- PyTorch/util/evaluation_functions.py
+++ PyTorch/util/evaluation_functions.py
@@ -35,6 +35,6 @@ def evaluate_regression_model(model: nn.Module, criterion, dataloader, name='mod
 
 def load_weights(model, model_path):
     checkpoint = torch.load(model_path, map_location=torch.device('cpu'))
-    model.load_state_dict(checkpoint)
+    model.load_state_dict(checkpoint, strict=False)
 
     return model
